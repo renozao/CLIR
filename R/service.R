51ed8312ec60709e8250813d7543a21786f38a97
Renaud <renaud@tx.technion.ac.il>
2016-12-28 14:31:51 +0200

    Fixed bug in finding service file when using development packages

--- R/service.R
+++ R/service.R
@@ -6,13 +6,56 @@
 
 #' Path to Report File
 #' 
+#' @param name name of the service.
+#' It can be the name of the script that implements the service or the name of the directory
+#' that contains it.
+#' @param all logical that indicates if all the files associated with the service should be 
+#' returned.
+#' @param full.names logical that indicates if the full path to the services file(s) should 
+#' be returned or only the path relative to the service base directory.
+#' The base directory itself is always returned as an attribute ('path').
+#' @param package name of the package where to look for the givne service.
+#' 
+#' @importFrom pkgmaker packagePath
 #' @export 
-service_file <- function(pattern, package = NULL){
+service_files <- function(name, all = FALSE, full.names = FALSE, package = topenv(parent.frame())){
   
-  pe <- parent.frame()
-  package <- package %||% packageName(topenv(pe))
-  sfiles <- list.files(system.file('services', package = package), pattern = pattern, full.names = TRUE)
-  sfiles[1L]
+  serv_path <- packagePath('services', package = package)
+  serv_base <- file.path(serv_path, name)
+  # extend to actual file if one found the given service directory
+  if( dir.exists(serv_base) ){
+    serv_files <- sort(list.files(serv_base, include.dirs = TRUE))
+    # find suitable scripts and put them first
+    exec_files <- grep("\\.r$", serv_files, ignore.case = TRUE, value = TRUE)
+    serv_files <- serv_files[order(match(serv_files, exec_files))]
+    
+    if( !all ) serv_files <- serv_files[1L] 
+  
+  }else{
+    if( file_ext(serv_base) == '' && file.exists(serv_file_r <- paste0(serv_base, ".r")) ) serv_files <- serv_file_r
+    else{
+      if( !file.exists(serv_base) ) stop("Could not find service '", name, "' under ", serv_base)
+      serv_files <- serv_base
+    }
+    serv_base <- dirname(serv_files)
+  }
+  
+  # remove base part if not requested otherwise
+  if( !full.names ) serv_files <- sub(paste0(serv_base, '/'), "", serv_files, fixed = TRUE)
+  
+  attr(serv_files, 'path') <- serv_base
+  serv_files
+  
+}
+
+# copies all the files needed for running a service to a target directory
+copy_service_files <- function(name, to, ...){
+  
+  sfiles <- service_files(name, ..., full.names = FALSE, all = TRUE)
+  base <- attr(sfiles, 'path')
+  file.copy(file.path(base, sfiles), to, recursive = TRUE, overwrite = TRUE)
+  sfiles
+  
 }
 
 #' Renders Service Report File
@@ -24,38 +67,43 @@ service_file <- function(pattern, package = NULL){
 #' @param args report parameters passed as a character vector, typically from command line arguments.
 #' If \var{args} is not \code{NULL}, then any argument passed in \var{...} is ignored.
 #' @param work_dir path to working directory
-#' @param help logical that requests the service man page to be shown.
-#' If \code{TRUE}, the service is not run at all, and only show the man page before exiting.
+#' @param save logical or character string that specify if or where the result list should be saved.
 #' @param envir environment where the report is rendered. See \code{\link[rmarkdown]{render}}.
+#' @inheritParams service_files
 #'  
 #' @seealso \code{\link[rmarkdown]{render}}
 #' @export
-render_service <- function(service, ..., args = NULL, work_dir = '.', save = FALSE, envir = parent.frame(), package = NULL){
+render_service <- function(name, ..., args = NULL, work_dir = '.', save = FALSE, envir = parent.frame(), package = topenv(parent.frame())){
   
   if( !requireNamespace('rmarkdown') )
     stop('Missing dependency: package "rmarkdown" is needed to render service documents.')
   
-  # retrieve service file
-  pe <- parent.frame()
-  package <- package %||% packageName(topenv(pe))
-  service_file <- service_file(service, package = package)
-  
-  # setup working directory
+  # setup working directory with service files
   dir.create(work_dir, recursive = TRUE, showWarnings = FALSE)
-  file.copy(service_file, work_dir, overwrite = TRUE)
+  work_dir <- normalizePath(work_dir)
+  
+  # copy service files
+  sfiles <- copy_service_files(name, to = work_dir, package = package)
+  service_file <- sfiles[1L]
+  # change to output directory
   owd <- setwd(work_dir)
-  on.exit( setwd(owd) )
+  # setup cleanup on exit
+  on.exit({
+    # delete service files
+    unlink(file.path(work_dir, sfiles), recursive = TRUE)
+    # switch back to old working directory
+    setwd(owd) 
+  })
   
   # render service script
   PARAMS <- list(...)
-  report_file <- rmarkdown::render(basename(service_file), params = PARAMS, envir = envir)
+  report_file <- rmarkdown::render(service_file, params = PARAMS, envir = envir)
   
   # return result
   result <- list(report_file = report_file, params = PARAMS)
   
   # retrieve output data
   output <- yaml_header(service_file, 'return')
-  print(ls(envir))
   if( length(output) ){
     if( is.list(output) ) output <- names(output)
     res <- sapply(output, function(n) get0(n, envir = envir, inherits = FALSE), simplify = FALSE)
